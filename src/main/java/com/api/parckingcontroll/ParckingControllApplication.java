8a7f2759cc74f7d5349e056e5bc35b7f9678ef73
Clebson Santos <clebsonsantosmtt@gmail.com>
2022-12-01 15:04:17 -0300

    initial setup spring

---
+++ src/main/java/com/api/parckingcontroll/ParckingControllApplication.java
@@ -0,0 +1,13 @@
+package com.api.parckingcontroll;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ParckingControllApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ParckingControllApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/api/parckingcontroll/ParckingControllApplicationTests.java
@@ -0,0 +1,13 @@
+package com.api.parckingcontroll;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ParckingControllApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
